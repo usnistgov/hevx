f535c74e2856ad0bee098a04ae7126efb20ee528
Wesley Griffin <wgriffin@fraktured.net>
2018-09-12 16:08:29 -0400

    Try to fix build on GCC

--- iris/protos.h
+++ iris/protos.h
@@ -6,12 +6,18 @@
 #if PLATFORM_COMPILER_MSVC
 #pragma warning(push)
 #pragma warning(disable : 4100)
-#endif // PLATFORM_COMPILER_MSVC
+#elif PLATFORM_COMPILER_GCC
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
 
 #include "iris/protos/window.pb.h"
 
 #if PLATFORM_COMPILER_MSVC
 #pragma warning(pop)
-#endif // PLATFORM_COMPILER_MSVC
+#elif PLATFORM_COMPILER_GCC
+#pragma GCC diangostic push
+#endif
 
-#endif // HEV_IRIS_PROTOS_H_
+#endif
