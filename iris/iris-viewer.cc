407293ebf94cd51343002501870423256169df6f
Wesley Griffin <wesley.griffin@nist.gov>
2019-01-29 13:59:41 -0500

    Add a log widget to iris-viewer

--- iris/iris-viewer.cc
+++ iris/iris-viewer.cc
@@ -10,6 +10,7 @@
 #pragma warning(push)
 #pragma warning(disable : 4127)
 #endif
+#include "spdlog/sinks/base_sink.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
@@ -17,6 +18,71 @@
 #pragma warning(pop)
 #endif
 #include "flags.h"
+#include <mutex>
+
+template <class Mutex>
+class iris_viewer_sink : public spdlog::sinks::base_sink<Mutex> {
+  ImGuiTextBuffer textBuffer_{};
+  ImVector<int> lineOffsets_{};
+  bool scrollToBottom_{false};
+
+public:
+  void Draw() {
+    if (ImGui::Button("Clear")) {
+      textBuffer_.clear();
+      lineOffsets_.clear();
+      lineOffsets_.push_back(0);
+    }
+
+    ImGui::Separator();
+    ImGui::BeginChild("scrolling", ImVec2(0, 0), false,
+                      ImGuiWindowFlags_HorizontalScrollbar);
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+
+    char const* bufferStart = textBuffer_.begin();
+    char const* bufferEnd = textBuffer_.end();
+
+    ImGuiListClipper clipper;
+    clipper.Begin(lineOffsets_.Size);
+
+    while (clipper.Step()) {
+      for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; ++i) {
+        char const* lineStart = bufferStart + lineOffsets_[i];
+        char const* lineEnd = (i + 1 < lineOffsets_.Size)
+                                ? (bufferStart + lineOffsets_[i + 1] - 1)
+                                : bufferEnd;
+        ImGui::TextUnformatted(lineStart, lineEnd);
+      }
+    }
+    clipper.End();
+
+    ImGui::PopStyleVar();
+    if (scrollToBottom_) ImGui::SetScrollHereY(1.f);
+    scrollToBottom_ = false;
+    ImGui::EndChild();
+  }
+
+protected:
+  void sink_it_(spdlog::details::log_msg const& msg) override {
+    fmt::memory_buffer formatted;
+    spdlog::sinks::sink::formatter_->format(msg, formatted);
+
+    int oldSize = textBuffer_.size();
+    textBuffer_.append(formatted.begin(), formatted.end());
+
+    for (int newSize = textBuffer_.size(); oldSize < newSize; ++oldSize) {
+      if (textBuffer_[oldSize] == '\n') lineOffsets_.push_back(oldSize + 1);
+    }
+
+    scrollToBottom_ = true;
+  } // sink_it_
+
+  void flush_() override {
+  } // flush_
+}; // class iris_viewer_sink
+
+using iris_viewer_sink_mt = iris_viewer_sink<std::mutex>;
+using iris_viewer_sink_st = iris_viewer_sink<spdlog::details::null_mutex>;
 
 #if PLATFORM_WINDOWS
 extern "C" {
@@ -59,7 +125,10 @@ int main(int argc, char** argv) {
   auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
   console_sink->set_level(spdlog::level::trace);
 
-  spdlog::logger logger("iris-viewer", {console_sink, file_sink});
+  auto ui_sink = std::make_shared<iris_viewer_sink_mt>();
+  ui_sink->set_level(spdlog::level::warn);
+
+  spdlog::logger logger("iris-viewer", {console_sink, file_sink, ui_sink});
   logger.set_level(spdlog::level::trace);
 
   logger.info("initialized");
@@ -68,7 +137,7 @@ int main(int argc, char** argv) {
         "iris-viewer",
         iris::Renderer::Options::kReportDebugMessages |
           iris::Renderer::Options::kUseValidationLayers,
-        0, {console_sink, file_sink}); error.code()) {
+        0, {console_sink, file_sink, ui_sink}); error.code()) {
     logger.critical("cannot initialize renderer: {}", error.what());
     std::exit(EXIT_FAILURE);
   }
@@ -95,6 +164,8 @@ int main(int argc, char** argv) {
         "Frame Times", frameTimes.data(), frameTimes.size(), 0,
         fmt::format("Average {:.3f} ms", 1000.f / io.Framerate).c_str(), 0.f,
         100.f, ImVec2(0, 50));
+
+      ui_sink->Draw();
     }
     ImGui::End(); // Status
 
