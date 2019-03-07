4141db62842896f2e1296aea07447eae67458b5d
Wesley Griffin <wesley.griffin@nist.gov>
2019-03-07 14:37:58 -0500

    Refactor the CMake bits some

--- third_party/Glslang.cmake
+++ third_party/Glslang.cmake
@@ -2,6 +2,10 @@ set(url https://github.com/KhronosGroup/glslang)
 set(commit 5432f0dd8f331f15182681664d7486681e8514e6) # sdk-1.1.101.0
 
 if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/glslang/CMakeLists.txt)
+  if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/glslang)
+    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/glslang)
+  endif()
+
   execute_process(
     COMMAND ${GIT_EXECUTABLE} clone ${url} glslang
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
@@ -21,7 +25,7 @@ if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/glslang/CMakeLists.txt)
   endif()
 
   execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} update_glslang_sources.py
+    COMMAND ${Python3_EXECUTABLE} update_glslang_sources.py
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/glslang
     RESULT_VARIABLE res OUTPUT_VARIABLE out ERROR_VARIABLE out
   )
