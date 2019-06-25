0e78e04152db7315f5f2f3026088eda06a142fb5
Wesley Griffin <wesley.griffin@nist.gov>
2019-06-25 19:51:40 -0400

    Peg expected to a previous commit.

--- third_party/Expected.cmake
+++ third_party/Expected.cmake
@@ -1,10 +1,12 @@
+set(_expected_git_tag 1fb40294e6fc8c147289eeb55a6d42b007659033)
+
 set(EXPECTED_ENABLE_DOCS OFF CACHE BOOL "" FORCE)
 set(EXPECTED_ENABLE_TESTS ${BUILD_DEPENDENCY_TESTING} CACHE BOOL "" FORCE)
 
 message(STATUS "Populating expected")
 FetchContent_Populate(expected
   GIT_REPOSITORY https://github.com/TartanLlama/expected
-  GIT_SHALLOW TRUE # Expected "should be" stable at HEAD
+  GIT_SHALLOW TRUE GIT_TAG ${_expected_git_tag}
   PATCH_COMMAND
     ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_SOURCE_DIR}/ExpectedPatch.cmake
   SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/expected
