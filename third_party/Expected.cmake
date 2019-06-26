c69d0350636b648772e112c1faca271374c80d8f
Wesley Griffin <wesley.griffin@nist.gov>
2019-06-25 21:03:12 -0400

    Turn on shallow clones for expected

--- third_party/Expected.cmake
+++ third_party/Expected.cmake
@@ -6,7 +6,7 @@ set(EXPECTED_ENABLE_TESTS ${BUILD_DEPENDENCY_TESTING} CACHE BOOL "" FORCE)
 message(STATUS "Populating expected")
 FetchContent_Populate(expected
   GIT_REPOSITORY https://github.com/TartanLlama/expected
-  GIT_SHALLOW TRUE GIT_TAG ${_expected_git_tag}
+  GIT_SHALLOW FALSE GIT_TAG ${_expected_git_tag}
   PATCH_COMMAND
     ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_SOURCE_DIR}/ExpectedPatch.cmake
   SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/expected
