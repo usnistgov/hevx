d64712a9c1e274eb540cbf73b8fcb8930953bfe4
Wesley Griffin <wesley.griffin@nist.gov>
2019-06-25 21:36:35 -0400

    Fork expected and tag previously working commit.

--- third_party/Expected.cmake
+++ third_party/Expected.cmake
@@ -1,14 +1,14 @@
-set(_expected_git_tag v1.0.0)
+set(_expected_git_tag hevx)
 
 set(EXPECTED_ENABLE_DOCS OFF CACHE BOOL "" FORCE)
 set(EXPECTED_ENABLE_TESTS ${BUILD_DEPENDENCY_TESTING} CACHE BOOL "" FORCE)
 
 message(STATUS "Populating expected")
 FetchContent_Populate(expected
-  GIT_REPOSITORY https://github.com/TartanLlama/expected
+  GIT_REPOSITORY https://github.com/wesleygriffin/expected
   GIT_SHALLOW TRUE GIT_TAG ${_expected_git_tag}
-  #PATCH_COMMAND
-    #${CMAKE_COMMAND} -P ${CMAKE_CURRENT_SOURCE_DIR}/ExpectedPatch.cmake
+  PATCH_COMMAND
+    ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_SOURCE_DIR}/ExpectedPatch.cmake
   SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/expected
   UPDATE_DISCONNECTED ${THIRD_PARTY_UPDATE_DISCONNECTED}
   QUIET
