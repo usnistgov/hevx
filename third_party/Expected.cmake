e3e4b4e114a7ccf2339b98d35722e37d1811a8fb
Wesley Griffin <wesley.griffin@nist.gov>
2019-06-25 21:21:51 -0400

    Don't patch expected

--- third_party/Expected.cmake
+++ third_party/Expected.cmake
@@ -6,9 +6,9 @@ set(EXPECTED_ENABLE_TESTS ${BUILD_DEPENDENCY_TESTING} CACHE BOOL "" FORCE)
 message(STATUS "Populating expected")
 FetchContent_Populate(expected
   GIT_REPOSITORY https://github.com/TartanLlama/expected
-  GIT_SHALLOW FALSE GIT_TAG ${_expected_git_tag}
-  PATCH_COMMAND
-    ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_SOURCE_DIR}/ExpectedPatch.cmake
+  GIT_SHALLOW TRUE GIT_TAG ${_expected_git_tag}
+  #PATCH_COMMAND
+    #${CMAKE_COMMAND} -P ${CMAKE_CURRENT_SOURCE_DIR}/ExpectedPatch.cmake
   SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/expected
   UPDATE_DISCONNECTED ${THIRD_PARTY_UPDATE_DISCONNECTED}
   QUIET
